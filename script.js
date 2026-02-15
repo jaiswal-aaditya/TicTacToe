2123436449484c7802a9404a45244aa933186ad1
Aaditya JaiswaL <261752131+aadityajaiswal36@users.noreply.github.com>
2026-02-15 19:40:28 +0530

    Complete TicTacToe web app

---
+++ script.js
@@ -0,0 +1,109 @@
+console.log("Welcome to TicTacToe")
+const music = document.getElementById("bg-music");
+const btn = document.getElementById("music-btn");
+const resetBtn = document.getElementById("reset");
+const hamburger = document.querySelector(".hamburger");
+const Close = document.querySelector(".close");
+const navLinks = document.querySelector(".nav-links");
+let boxes = document.getElementsByClassName("box");
+let turn = "X";
+let isPlaying = false;
+let gameOver = false;
+
+btn.addEventListener("click", () => {
+    if (!isPlaying) {
+        music.play();
+        btn.textContent = "🔇 Stop Music";
+    }
+    else {
+        music.pause();
+        btn.textContent = "🔊 Play Music";
+    }
+    isPlaying = !isPlaying;
+});
+
+const checkWin = () => {
+    let imgboxes = document.querySelectorAll(".containers img");
+    let wins = [
+        [0, 1, 2, 50, -2100, 0, 1],
+        [3, 4, 5, 50, 0, 0, 1],
+        [6, 7, 8, 50, 2000, 0, 1],
+        [0, 3, 6, 8.3333, 0, 90, 1],
+        [1, 4, 7, 50, 0, 90, 1],
+        [2, 5, 8, 91.6666, 0, 90, 1],
+        [0, 4, 8, 50, 0, 45, 1.4],
+        [2, 4, 6, 50, 0, 135, 1.4]
+    ]
+    wins.forEach(e => {
+        if ((imgboxes[e[0]].alt === imgboxes[e[1]].alt) && (imgboxes[e[1]].alt === imgboxes[e[2]].alt) && (imgboxes[e[0]].alt !== "")) {
+            document.querySelector(".win-line").style.transform = `translate(${e[3]}%, ${e[4]}%) rotate(${e[5]}deg) scaleX(${e[6]})`;
+
+            document.querySelector(".info span").innerText = "Won:  ";
+            document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "45%";
+            boxes[e[0]].classList.add("win-box")
+            boxes[e[1]].classList.add("win-box")
+            boxes[e[2]].classList.add("win-box")
+            gameOver = true;
+        }
+    });
+    if (!gameOver) {
+        let isFull = true;
+        for (let i = 0; i < imgboxes.length; i++) {
+            if (imgboxes[i].alt === "") {
+                isFull = false; // Found an empty box, not a draw yet
+                break;
+            }
+        }
+        if (isFull) {
+            gameOver = true;
+            document.querySelector(".info span").innerText = "Draw: ";
+        }
+    };
+};
+
+Array.from(boxes).forEach(element => {
+    element.addEventListener("click", () => {
+        if (gameOver) return;
+        const img = element.querySelector("img");
+        const player = document.querySelector(".info img");
+        if (img.hidden) {
+            img.src = turn === "X" ? "assets/img/triangle.webp" : "assets/img/circle.webp";
+            img.alt = turn;
+            img.hidden = false;
+            turn = turn === "X" ? "O" : "X";
+            checkWin();
+            if (!gameOver) {
+                player.src = turn === "X" ? "assets/img/triangle-guard.svg" : "assets/img/circle-guard.svg";
+                player.alt = turn;
+            }
+        }
+    })
+});
+
+resetBtn.addEventListener("click", () => {
+    Array.from(boxes).forEach(box => {
+        const img = box.querySelector("img");
+        img.src = "";
+        img.alt = "";
+        img.hidden = true;
+        box.classList.remove("win-box");
+    });
+    turn = "X";
+    gameOver = false;
+    document.querySelector(".info img").src = "assets/img/triangle-guard.svg";
+    document.querySelector(".info img").alt = turn;
+    document.querySelector(".info span").innerText = "Turn: ";
+    document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "0";
+    document.querySelector(".win-line").style.transform = "scaleX(0.00000000000000000001)";
+});
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
+    Close.classList.toggle("active");
+});
+Close.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    hamburger.classList.remove("active");
+    Close.classList.remove("active");
+});
